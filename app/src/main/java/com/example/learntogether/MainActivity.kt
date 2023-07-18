3339a8c3744452c89f60948249e0e1fe715a7174
ntwigamartin <ntwiga.martin@gmail.com>
2023-07-18 15:33:51 +0300

    Completed homepage

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomePage(
+                        stringResource(R.string.article_heading),
+                        stringResource(R.string.article_first_paragraph),
+                        stringResource(R.string.article_second_paragraph)
+                    )
                 }
             }
         }
@@ -30,17 +44,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomePage(heading: String, paragraph1: String, paragraph2: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    val defaultFontSize = 18.sp
+    val defaultTextStyle = TextStyle(fontSize = defaultFontSize)
+
+    Column(
+        modifier = modifier,
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = heading,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = paragraph1,
+            style = defaultTextStyle,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = paragraph2,
+            style = defaultTextStyle,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LearnTogetherPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        HomePage(
+            stringResource(R.string.article_heading),
+            stringResource(R.string.article_first_paragraph),
+            stringResource(R.string.article_second_paragraph)
+        )
+
+
+
     }
 }
